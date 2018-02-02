e5dd1943c11d7f62f2913ecf568a14cf849d3cfd
James Cabeso <james.cabeso@urbanmassage.com>
2018-02-02 11:48:44 +0000

    More changes to Object and Methods

--- udemyTutorials.js
+++ udemyTutorials.js
@@ -5,23 +5,34 @@
 var james = {
     name: 'Billy',
     lastName: 'Midget',
-    yearOfBirth: 35,
+    yearOfBirth: 1990,
     job: 'CEO AMAGON',
     isMarried: false,
     cars: ['Bently', 'Audi R8', 'Rolls Royce'],
     //function expressions: a variable inside an object which is assign to a function
-    calculateAge: function(yearOfBirth){
-      return 2018 - yearOfBirth;
+    calculateAge: function()/*(yearOfBirth)*/ {
+//this refers to the object 'john' that has to calculate the method
+      return 2018 - this.yearOfBirth /*yearOfBirth;*/
+
     }
 };
+/////
+//part2 - how to use this line inside the METHOD/Object
 
+//console.log(james.calculateAge(1930));
+//This line will simply return the function calculateAge
+console.log(james.calculateAge());
 
+//this piece of code adds the var age 'calculatAge' and puts this code within the object
+var age = james.calculateAge();
+james.age = age;
 
-//this is to call the variable 'james' and return everything within the object
 console.log(james);
+////////////////
+//part 1
+//this is to call the variable 'james' and return everything within the object
+//console.log(james);
 //this is to call the property 'lastName' and ruturn 'Midget'
-console.log(james.lastName);
+//console.log(james.lastName);
 //this is to call the property 'cars' and the index 2 and return 'Rolls royce'
-console.log(james.cars[2]);
-
-console.log(james.calculateAge(1930));
+//console.log(james.cars[2]);
